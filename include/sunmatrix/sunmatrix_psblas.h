fd8c69ea716e929d71288bdd617a3ef3cd618aa6
Luca Pepè Sciarria <luca.pepesciarria@gmail.com>
2024-11-04 15:24:57 +0100

    new feature: add sunmatrix implementation through PSCTOOLKIT (psblas)

---
+++ include/sunmatrix/sunmatrix_psblas.h
@@ -0,0 +1,105 @@
+/*
+ * -----------------------------------------------------------------
+ * Programmer(s): F. Durastante @ IAC-CNR
+ * Based on code sundials_sparse.h by: Carol Woodward and
+ *     Slaven Peles @ LLNL, and Daniel R. Reynolds @ SMU
+ * -----------------------------------------------------------------
+ * SUNDIALS Copyright Start
+ * Copyright (c) 2002-2019, Lawrence Livermore National Security
+ * and Southern Methodist University.
+ * All rights reserved.
+ *
+ * See the top-level LICENSE and NOTICE files for details.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SUNDIALS Copyright End
+ * -----------------------------------------------------------------
+ * This is the header file for the psblas implementation of the
+ * SUNMATRIX module, SUNMATRIX_PSBLAS.
+ *
+ * Notes:
+ *   - The definition of the generic SUNMatrix structure can be found
+ *     in the header file sundials_matrix.h.
+ *   - The definition of the type 'sunrealtype' can be found in the
+ *     header file sundials_types.h, and it may be changed (at the
+ *     configuration stage) according to the user's needs.
+ *     The sundials_types.h file also contains the definition
+ *     for the type 'booleantype' and 'indextype'.
+ * -----------------------------------------------------------------
+ */
+
+#ifndef _SUNMATRIX_PSBLAS_H
+#define _SUNMATRIX_PSBLAS_H
+
+#include <stdio.h>
+#include <mpi.h>
+#include "psb_base_cbind.h"
+#include "psb_c_dbase.h"
+#include "psb_util_cbind.h"
+#include <sundials/sundials_matrix.h>
+#include <sunmatrix/sunmatrix_dense.h>
+#include <sunmatrix/sunmatrix_band.h>
+#include <sunmatrix/sunmatrix_sparse.h>
+
+#undef I
+
+#ifdef __cplusplus  /* wrapper to enable C++ usage */
+extern "C" {
+#endif
+
+/* ------------------------------------------
+ * Sparse Implementation of SUNMATRIX_SPARSE
+ * ------------------------------------------ */
+
+struct _SUNMatrixContent_PSBLAS {
+  psb_c_descriptor *cdh;       /* descriptor for data distribution */
+  psb_c_dspmat  *ah;           /* PSBLAS sparse matrix             */
+  psb_c_ctxt *cctxt;           /* PSBLAS communicator              */
+};
+
+typedef struct _SUNMatrixContent_PSBLAS *SUNMatrixContent_PSBLAS;
+
+
+/* ---------------------------------------
+ * Macros for access to SUNMATRIX_SPARSE
+ * --------------------------------------- */
+
+#define SM_CONTENT_P(A)     ( (SUNMatrixContent_PSBLAS)(A->content) )
+
+#define SM_DESCRIPTOR_P(A)  ( SM_CONTENT_P(A)->cdh )
+
+#define SM_PMAT_P(A)        ( SM_CONTENT_P(A)->ah )
+
+#define SM_CCTXT_P(A)       ( SM_CONTENT_P(A)->cctxt )
+
+/* ----------------------------------------
+ * Exported Functions for SUNMATRIX_PSBLAS
+ * ---------------------------------------- */
+
+SUNDIALS_EXPORT SUNMatrix SUNPSBLASMatrix(psb_c_ctxt *cctxt, psb_c_descriptor *cdh);
+
+SUNDIALS_EXPORT void SUNPSBLASMatrix_Print(SUNMatrix A, char *matrixtitle, char* filename);
+SUNDIALS_EXPORT sunindextype SUNPSBLASMatrix_Rows(SUNMatrix A);
+SUNDIALS_EXPORT sunindextype SUNPSBLASMatrix_Columns(SUNMatrix A);
+SUNDIALS_EXPORT sunindextype SUNPSBLASMatrix_NNZ(SUNMatrix A);
+SUNDIALS_EXPORT SUNMatrix_ID SUNMatGetID_PSBLAS(SUNMatrix A);
+SUNDIALS_EXPORT SUNMatrix SUNMatClone_PSBLAS(SUNMatrix A);
+SUNDIALS_EXPORT void SUNMatDestroy_PSBLAS(SUNMatrix A);
+SUNDIALS_EXPORT int SUNMatZero_PSBLAS(SUNMatrix A);
+SUNDIALS_EXPORT int SUNMatCopy_PSBLAS(SUNMatrix A, SUNMatrix B);
+SUNDIALS_EXPORT int SUNMatScaleAdd_PSBLAS(sunrealtype c, SUNMatrix A, SUNMatrix B);
+SUNDIALS_EXPORT int SUNMatScaleAddI_PSBLAS(sunrealtype c, SUNMatrix A);
+SUNDIALS_EXPORT int SUNMatMatvec_PSBLAS(SUNMatrix A, N_Vector x, N_Vector y);
+SUNDIALS_EXPORT int SUNMatSpace_PSBLAS(SUNMatrix A, long int *lenrw, long int *leniw);
+/* ----------------------------------------
+ * Auxiliary Functions for SUNMATRIX_PSBLAS
+ * ---------------------------------------- */
+SUNDIALS_EXPORT int SUNMatAsb_PSBLAS(SUNMatrix A);
+SUNDIALS_EXPORT int SUNMatIns_PSBLAS(psb_i_t nz, const psb_l_t *irw, const psb_l_t *icl,
+ 			const psb_d_t *val,SUNMatrix A);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/sunmatrix/psblas/sunmatrix_psblas.c
@@ -0,0 +1,331 @@
+/*
+ * -----------------------------------------------------------------
+ * Programmer(s): F. Durastante @ IAC-CNR
+ * Based on code sundials_sparse.c by: Carol Woodward and
+ *     Slaven Peles @ LLNL, and Daniel R. Reynolds @ SMU
+ * -----------------------------------------------------------------
+ * SUNDIALS Copyright Start
+ * Copyright (c) 2002-2019, Lawrence Livermore National Security
+ * and Southern Methodist University.
+ * All rights reserved.
+ *
+ * See the top-level LICENSE and NOTICE files for details.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SUNDIALS Copyright End
+ * -----------------------------------------------------------------
+ * This is the implementation file for the PSBLAS implementation of
+ * the SUNMATRIX package.
+ * -----------------------------------------------------------------
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sunmatrix/sunmatrix_psblas.h>
+#include <nvector/nvector_psblas.h>
+#include <sundials/sundials_nvector.h>
+#include <sundials/sundials_math.h>
+
+#define ZERO (psb_d_t) 0.0
+#define ONE  (psb_d_t) 1.0
+
+/*
+ * -----------------------------------------------------------------
+ * exported functions
+ * -----------------------------------------------------------------
+ */
+
+/*
+ * ==================================================================
+ * Private function prototypes (functions working on SlsMat)
+ * ==================================================================
+ */
+
+/* ----------------------------------------------------------------------------
+ * Function to create a new sparse matrix
+ */
+
+SUNMatrix SUNPSBLASMatrix(psb_c_ctxt *cctxt, psb_c_descriptor *cdh)
+{
+  SUNMatrix A;
+  SUNMatrix_Ops ops;
+  SUNMatrixContent_PSBLAS content;
+  psb_c_dspmat *ah;
+  psb_i_t ret;
+
+  /* Create Matrix */
+  A = NULL;
+  A = (SUNMatrix) malloc(sizeof *A);
+  if (A == NULL) return(NULL);
+
+  /* Create matrix operation structure */
+  ops = NULL;
+  ops = (SUNMatrix_Ops) malloc(sizeof(struct _generic_SUNMatrix_Ops));
+  if (ops == NULL) { free(A); return(NULL); }
+
+  /* Attach operations */
+  ops->getid       = SUNMatGetID_PSBLAS;
+  ops->clone       = SUNMatClone_PSBLAS;
+  ops->destroy     = SUNMatDestroy_PSBLAS;
+  ops->zero        = SUNMatZero_PSBLAS;
+  ops->copy        = SUNMatCopy_PSBLAS;
+  ops->scaleadd    = SUNMatScaleAdd_PSBLAS;
+  ops->scaleaddi   = SUNMatScaleAddI_PSBLAS;
+  ops->matvec      = SUNMatMatvec_PSBLAS;
+  ops->space       = SUNMatSpace_PSBLAS;
+	ops->matvecsetup = NULL; // PSBLAS doesn't need seetup before matvec!
+
+  /* Create content */
+  content = NULL;
+  content = (SUNMatrixContent_PSBLAS) malloc(sizeof(struct _SUNMatrixContent_Sparse));
+  if (content == NULL) { free(ops); free(A); return(NULL); }
+
+  content->cctxt = cctxt;
+  content->cdh   = cdh;
+  content->ah    = psb_c_new_dspmat();
+  ret = psb_c_dspall(content->ah,cdh);
+  if(ret != 0){
+    free(content);
+    free(ops);
+    free(A);
+    return(NULL);
+  }
+
+  /* Attach content and ops */
+  A->content = content;
+  A->ops     = ops;
+
+  return(A);
+}
+
+/* ----------------------------------------------------------------------------
+ * Function to print the sparse matrix
+ */
+
+void SUNPSBLASMatrix_Print(SUNMatrix A, char *matrixtitle, char* filename)
+{
+  psb_c_dmm_mat_write( SM_PMAT_P(A), matrixtitle, filename);
+
+  return;
+}
+
+
+/* ----------------------------------------------------------------------------
+ * Functions to access the contents of the sparse matrix structure
+ */
+
+sunindextype SUNPSBLASMatrix_Rows(SUNMatrix A)
+{
+  if (SUNMatGetID(A) == SUNMATRIX_CUSTOM)
+    return(psb_c_dmat_get_nrows(SM_PMAT_P(A)));
+  else
+    return -1;
+}
+
+sunindextype SUNPSBLASMatrix_Columns(SUNMatrix A)
+{
+  if (SUNMatGetID(A) == SUNMATRIX_CUSTOM)
+    return(psb_c_dmat_get_ncols(SM_PMAT_P(A)));
+  else
+    return -1;
+}
+
+sunindextype SUNPSBLASMatrix_NNZ(SUNMatrix A)
+{
+  if (SUNMatGetID(A) == SUNMATRIX_CUSTOM)
+    return(psb_c_dnnz(SM_PMAT_P(A),SM_DESCRIPTOR_P(A)));
+  else
+    return -1;
+}
+
+/*
+ * -----------------------------------------------------------------
+ * implementation of matrix operations
+ * -----------------------------------------------------------------
+ */
+
+SUNMatrix_ID SUNMatGetID_PSBLAS(SUNMatrix A)
+{
+  return SUNMATRIX_CUSTOM;
+}
+
+SUNMatrix SUNMatClone_PSBLAS(SUNMatrix A)
+{
+  SUNMatrix B;
+  SUNMatrix_Ops ops;
+  SUNMatrixContent_PSBLAS content;
+  psb_c_dspmat *ah;
+  psb_i_t ret;
+
+  /* Create Matrix */
+  B = NULL;
+  B = (SUNMatrix) malloc(sizeof *A);
+  if (B == NULL) return(NULL);
+
+  /* Create matrix operation structure */
+  ops = NULL;
+  ops = (SUNMatrix_Ops) malloc(sizeof(struct _generic_SUNMatrix_Ops));
+  if (ops == NULL) { free(B); return(NULL); }
+
+  /* Attach operations */
+  ops->getid       = SUNMatGetID_PSBLAS;
+  ops->clone       = SUNMatClone_PSBLAS;
+  ops->destroy     = SUNMatDestroy_PSBLAS;
+  ops->zero        = SUNMatZero_PSBLAS;
+  ops->copy        = SUNMatCopy_PSBLAS;
+  ops->scaleadd    = SUNMatScaleAdd_PSBLAS;
+  ops->scaleaddi   = SUNMatScaleAddI_PSBLAS;
+  ops->matvec      = SUNMatMatvec_PSBLAS;
+  ops->space       = SUNMatSpace_PSBLAS;
+	ops->matvecsetup = NULL; // PSBLAS doesn't need setup before Matvec
+
+  /* Create content */
+  content = NULL;
+  content = (SUNMatrixContent_PSBLAS) malloc(sizeof(struct _SUNMatrixContent_Sparse));
+  if (content == NULL) { free(ops); free(A); return(NULL); }
+
+  content->cctxt = SM_CCTXT_P(A);
+  content->cdh   = SM_DESCRIPTOR_P(A);
+  ah  = psb_c_new_dspmat();
+  ret = psb_c_dspall(ah,SM_DESCRIPTOR_P(A));
+  if(ret != 0){
+    free(content);
+    free(ops);
+    free(B);
+    return(NULL);
+  }
+  content->ah = ah;
+
+  /* Attach content and ops */
+  B->content = content;
+  B->ops     = ops;
+
+  return(B);
+}
+
+void SUNMatDestroy_PSBLAS(SUNMatrix A)
+{
+  /* Free the PSBLAS Sparse Matrix */
+  psb_c_dspfree(SM_PMAT_P(A), SM_DESCRIPTOR_P(A));
+  /* Free the NMATRIX_PSBLAS Structure */
+	free(SM_PMAT_P(A));
+  free(A->content); A->content = NULL;
+  free(A->ops);  A->ops = NULL;
+  free(A); A = NULL;
+
+  return;
+}
+
+int SUNMatZero_PSBLAS(SUNMatrix A)
+{
+  bool clear = SUNTRUE;
+  // The PSBLAS matrix contained in A will be in the UPDATE state at the of
+  // this call
+  return(psb_c_dsprn(SM_PMAT_P(A), SM_DESCRIPTOR_P(A), clear));
+}
+
+int SUNMatCopy_PSBLAS(SUNMatrix A, SUNMatrix B)
+{
+  /* matrix should be in the ASSEMBLED state */
+	if(!psb_c_dis_matasb(SM_PMAT_P(A),SM_DESCRIPTOR_P(A)))
+		psb_c_dspasb(SM_PMAT_P(A),SM_DESCRIPTOR_P(A));
+
+  return(psb_c_dcopy_mat(SM_PMAT_P(A),SM_PMAT_P(B),SM_DESCRIPTOR_P(A)));
+}
+
+int SUNMatScaleAddI_PSBLAS(sunrealtype c, SUNMatrix A)
+{
+  /* matrix should be in the ASSEMBLED state */
+  if(!psb_c_dis_matasb(SM_PMAT_P(A),SM_DESCRIPTOR_P(A)))
+    psb_c_dspasb(SM_PMAT_P(A),SM_DESCRIPTOR_P(A));
+
+  return(psb_c_dspscalpid(c,SM_PMAT_P(A),SM_DESCRIPTOR_P(A)));
+}
+
+int SUNMatScaleAdd_PSBLAS(sunrealtype c, SUNMatrix A, SUNMatrix B)
+{
+
+	/* matrix should be in the ASSEMBLED state */
+	if(!psb_c_dis_matasb(SM_PMAT_P(A),SM_DESCRIPTOR_P(A)))
+		psb_c_dspasb(SM_PMAT_P(A),SM_DESCRIPTOR_P(A));
+
+	if (A == B){
+		/* If the routine is called with the same matrix argument
+		 	then A = c *A + A = (1+c)*A, so we use scaling routine
+			instead */
+		return(psb_c_dspscal(1.0+c, SM_PMAT_P(A), SM_DESCRIPTOR_P(A)));
+	}else{
+		/* matrix should be in the ASSEMBLED state */
+		if(!psb_c_dis_matasb(SM_PMAT_P(B),SM_DESCRIPTOR_P(B)))
+			psb_c_dspasb(SM_PMAT_P(B),SM_DESCRIPTOR_P(B));
+
+		return(psb_c_dspaxpby(c,SM_PMAT_P(A),ONE,SM_PMAT_P(B),SM_DESCRIPTOR_P(A)));
+	}
+
+}
+
+int SUNMatMatvec_PSBLAS(SUNMatrix A, N_Vector x, N_Vector y)
+{
+
+  /* matrix should be in the ASSEMBLED state */
+	if(!psb_c_dis_matasb(SM_PMAT_P(A),SM_DESCRIPTOR_P(A)))
+		psb_c_dspasb(SM_PMAT_P(A),SM_DESCRIPTOR_P(A));
+
+  return(psb_c_dspmm( (psb_d_t) 1.0, SM_PMAT_P(A), NV_PVEC_P(x) ,
+  		    (psb_d_t) 0.0, NV_PVEC_P(y) , SM_DESCRIPTOR_P(A)));
+}
+
+int SUNMatSpace_PSBLAS(SUNMatrix A, long int *lenrw, long int *leniw){
+  /* This function is advisory only, for use in determining a user's
+  total space requirements, for this module it is a dummy function
+  always returning true.*/
+
+	*lenrw = 0;
+	*leniw = 0;
+  return 0;
+}
+
+/*
+ * -----------------------------------------------------------------
+ * Auxiliary matrix operations
+ * -----------------------------------------------------------------
+ */
+
+ int SUNMatAsb_PSBLAS(SUNMatrix A){
+   /* Assemble a PSBLAS sparse matrix */
+  bool check;
+
+  if( A == NULL){
+    fprintf(stderr,"Error during matrix build loop for I\n");
+    return(1);
+  }
+
+  check = psb_c_dis_matasb(SM_PMAT_P(A),SM_DESCRIPTOR_P(A));
+
+    if(check){
+      return 0;
+    }else{
+      return(psb_c_dspasb(SM_PMAT_P(A),SM_DESCRIPTOR_P(A)));
+    }
+ }
+
+ int SUNMatIns_PSBLAS(psb_i_t nz, const psb_l_t *irw, const psb_l_t *icl,
+ 			const psb_d_t *val,SUNMatrix A){
+   /* This is just an overload of the psb_c_dspins function, can be used
+   to explicitly avoiding the user accessing the SM_PMAT_P(A) pointer */
+   bool check;
+
+   if( A == NULL){
+     fprintf(stderr,"Error during matrix build loop for I\n");
+     return(1);
+   }
+
+   check = psb_c_dis_matasb(SM_PMAT_P(A),SM_DESCRIPTOR_P(A));
+   /* If the matrix was already in ASSEMBLED state, it puts into UPDATE */
+   if(check){
+     psb_c_dset_matupd(SM_PMAT_P(A),SM_DESCRIPTOR_P(A));
+   }
+
+   return(psb_c_dspins(nz, irw, icl, val, SM_PMAT_P(A), SM_DESCRIPTOR_P(A)));
+ }
